cfe91d857ca3abc81fbcf838e2b4baa704872bad
Marti <Marti@10.189.145.238>
2014-10-01 18:06:10 +0200

    Maven project setup

---
+++ jersey-quickstart-webapp/src/main/java/org/glassfish/jersey/archetypes/jersey_quickstart_webapp/App.java
@@ -0,0 +1,13 @@
+package org.glassfish.jersey.archetypes.jersey_quickstart_webapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
